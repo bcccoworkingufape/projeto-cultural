c1a8bbfdcf4a1e0bd206736a3f2e48f25c7837d5
eemilyy <emilyalmeeidas@gmail.com>
2022-06-14 15:13:00 -0300

    alinhando itens

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -4,25 +4,27 @@ function login() {
     return (
         <>
         <div className='container col-4'>
-            <form className='d-flex justify-content-around'>
+            <form>
                 <div className='row'>
-                    <div className="form-group">
+                    <div className="form-group mt-4 ">
                         <label>Email</label>
                         <input type="email" className="form-control" />
                     </div>
-                    <div className="form-group">
+                    <div className="form-group mt-4 ">
                         <label>Senha</label>
                         <input type="password" className="form-control" />
                         <small className="form-text text-muted"><a href='?#'>Esqueceu sua senha?</a></small>
                     </div>
-                    <button type="submit" className="btn btn-primary">Entrar</button>
+                </div>
+                <div className='row w-100 m-0 mt-4'>
+                    <button type="submit" className="btn btn-primary shadow-sm p-2">Entrar</button>
                 </div>
             </form>
         </div>
 
         <div className='container col-4 mt-5'>
             <div className='align-items-center'>
-                <p>Ainda não tem conta?</p>
+                <p className='d-flex justify-content-center'>Ainda não tem conta?</p>
 
                 <div className='col'>
                     <button type="submit" className="btn btn-primary p-3 w-50">Sou Criador</button>
