e64b0e34f1486af0dc3f681493c0b6567f13a80a
eemilyy <emilyalmeeidas@gmail.com>
2022-06-15 17:06:44 -0300

    adicionando arquivo de cores e alterando o espaçamento de botoes no login

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -3,6 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function login() {
     return (
         <>
+         <div className='container col-4 mt-5'>
+            <p className='d-flex justify-content-center'>Login</p>
+
+            <div className='col'>
+                <button type="submit" className="btn btn-primary p-2 w-50">Entrar com Google</button>
+                <button type="submit" className="btn btn-primary p-2 w-50">Entrar com Facebook</button>
+            </div>
+
+            <hr />
+        </div>
+
+
         <div className='container col-4'>
             <form>
                 <div className='row'>
@@ -10,28 +22,28 @@ function login() {
                         <label>Email</label>
                         <input type="email" className="form-control" />
                     </div>
+
                     <div className="form-group mt-4 ">
                         <label>Senha</label>
                         <input type="password" className="form-control" />
-                        <small className="form-text text-muted"><a href='?#'>Esqueceu sua senha?</a></small>
+                        <small className="form-text"><a href='?#'>Esqueceu sua senha?</a></small>
                     </div>
                 </div>
+
                 <div className='row w-100 m-0 mt-4'>
-                    <button type="submit" className="btn btn-primary shadow-sm p-2">Entrar</button>
+                    <button type="submit" className="btn btn-primary p-2">Entrar</button>
                 </div>
             </form>
         </div>
 
         <div className='container col-4 mt-5'>
-            <div className='align-items-center'>
-                <p className='d-flex justify-content-center'>Ainda não tem conta?</p>
+            <p className='d-flex justify-content-center'>Ainda não tem conta?</p>
 
-                <div className='col'>
-                    <button type="submit" className="btn btn-primary p-3 w-50">Sou Criador</button>
-                    <button type="submit" className="btn btn-primary p-3 w-50">Sou Financiador</button>
-                </div>
+            <div className='col d-flex justify-content-between'>
+                <button type="submit" className="btn btn-primary p-2 w-50 mx-1">Sou Criador</button>
+                <button type="submit" className="btn btn-primary p-2 w-50 mx-1">Sou Financiador</button>
             </div>
-        </div>
+        </div>  
         </>
     );
   }
