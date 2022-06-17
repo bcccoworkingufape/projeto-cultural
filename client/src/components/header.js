bec895ddb54e3bfcbe6fa597feb63330d2913523
Michel Oliveira <micheltdoliveira@gmail.com>
2022-06-16 21:06:20 -0300

    Estilizando header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,32 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './header.scss';
 import logo from '../assets/images/logo.svg';
 
 function Header() {
     return (
-    <header className="p-3 bg-dark text-white">
-        <div className="container">
-            <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <a href="/" className="d-flex align-items-center mb-0 mb-lg-0 text-white text-decoration-none">
-                    <img src={logo} height= {30} alt="logo" />
-                </a>
+    <header className="bg-dark text-white">
+            <div className="container header d-flex align-items-center justify-content-center">
+                <div className='col-2'>
+                    <a href="/" className="d-flex align-items-center justify-content-center text-white text-decoration-none">
+                        <img src={logo} height= {30} alt="logo" />
+                    </a>
+                </div>
+                <div className="col-3 offset-3 d-flex ">
+                    <input type="text" className ="form-control col-11" placeholder="Busque um projeto"/>
+                    <button className ="btn btn-light submitButton" type="button">Buscar</button>
+                </div>
 
-                <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
- 
-                </ul>
-
-                <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-auto" role="search">
-                    <div>
-                    <input type="search" className="form-control form-control-light text-dark bg-light" placeholder="Busque um projeto" aria-label="Search"/>
-                    </div>
-
-
-                </form>
-
-                <div className="text-end">
-                <button type="button" className="btn btn-light">Entrar</button>
+                <div className="col-3 offset-3 d-flex">
+                    <button type="button" className="btn loginButton btn-light">Entrar</button>
                 </div>
             </div>
-        </div>
+
     </header>
     );
   }
