bf305b3c60fd7ff77643b2de26b26e7a721ef21f
Joanne Silva <joanneegabriela@gmail.com>
2022-06-16 23:37:52 -0300

    Criando migration de criaçao de usuario

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,10 +1,10 @@
+import { updateTypeAssertion } from 'typescript';
 import { appDataSource } from './data-source';
+import { User } from './entities/User';
 
 export default async (): Promise<void> => {
     return appDataSource.initialize()
-    .then(() =>
-        console.log("Banco de Dados incializado")
-    )
+    .then( () => console.log("Vanco de dados incializado") )
     .catch(error => console.log(`Ocorreu um erro durante a inicialização do banco de dados: ${error}`));
 }   
 
