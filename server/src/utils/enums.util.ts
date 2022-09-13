e15e4efdc7dbdf818be347775152cc24b49d152c
Joanne Silva <joanneegabriela@gmail.com>
2022-09-12 23:31:53 -0300

    Criando tabela de categorias de um projeto

---
+++ server/src/utils/enums.util.ts
@@ -0,0 +1,18 @@
+export enum StatusActive {
+    ACTIVE = 'ACTIVE',
+    INACTIVE = 'INACTIVE',
+}
+
+export enum StatusDelected {
+    NOT_DELETED = 'NOT_DELETED',
+    DELETED = 'DELETED',
+}
+
+export enum UserType {
+    ARTIST = 'ARTIST',
+    BUSSNESS_PERSON = 'BUSSNESS_PERSON',
+}
+
+export enum ErrorCode {
+    
+}
