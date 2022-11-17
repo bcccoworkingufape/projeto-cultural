857e6fe6bfa4157eb29e236cced1bc6faa360357
Joanne Silva <joanneegabriela@gmail.com>
2022-11-17 01:38:05 -0300

    Histórias do usuário: edição e visualização de projetos

--- server/src/controllers/Project.controller.ts
+++ server/src/controllers/Project.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import * as projectRepository from '../database/repositories/Project.repository';
+import { decodeLoginToken } from '../services/authService.service';
 
 export const create = async (req: Request, res: Response) => {
 	try {
@@ -15,13 +16,37 @@ export const create = async (req: Request, res: Response) => {
 	}
 };
 
+export const getById = async(req: Request, res: Response, nextFunction: NextFunction) => {
+	try {
+		const project = await projectRepository.getById(req.body.id);
+		res.status(200).send({ project });
+	} catch (error: any) {
+		res.status(400).send({ message: 'The request has failed: ' + error });
+	}
+}
+
+export const update = async(req: Request, res: Response, NextFunction: NextFunction) => {
+	try {
+		const project = await projectRepository.getById(req.body.id);
+		const token = req.headers.authorization;
+		const userId = (await decodeLoginToken(token)).id;
+
+		if(project.userId === userId) {
+			await projectRepository.update(req.body.id, req.body.projectData);
+			res.status(200).send({ projectId: project.id })
+		} 
+		else res.status(401).send({ message: 'Unauthorized user' });
+	} catch(error: any) {
+		res.status(400).send({ message: 'The request has failed: ' + error });
+	}
+}
+
+// verificar se o usuário que está fazendo a requisição é o dono do projeto 
 export const remove = async (req: Request, res: Response) => {
 	try {
-		if (!!!(await projectRepository.getByName(req.body.name))) throw new Error("Name could not be found");
+		if (!!!(await projectRepository.getById(req.body.id))) throw new Error("Non existent project");
 		else {
-			const project = await projectRepository.deleteProject({
-				name: req.body.name
-			});
+			const project = await projectRepository.deleteProject(req.body.id);
             		res.status(200).send({ project });
 		}
 	} catch (error: any) {
---
+++ server/src/interfaces/Project.interface.ts
@@ -0,0 +1,9 @@
+export interface Project {
+    id: string;
+    name: string;
+    description: string;
+    externalURL: string;
+    imageURL: string;
+    videoURL: string;
+    status: string;
+}
