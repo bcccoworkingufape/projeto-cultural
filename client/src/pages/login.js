29c9fda954eb9743665e49e2199e685566e60d4e
Lucas Branco Alves de Melo <lucasbranco027@protonmail.com>
2022-12-02 11:50:39 -0300

    pagina de login quase pronta. novas cores e fontes nos arquivos

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,55 +1,63 @@
 /*import 'bootstrap/dist/css/bootstrap.min.css';*/
 import { useNavigate } from 'react-router-dom';
+import { useRef } from 'react';
 import logo from '../assets/images/logo.svg';
 import './login.scss';
 import '../stylesheets/_colors.scss';
 import '../stylesheets/_fonts.scss';
-import LoginNavButton from '../components/buttons/loginNavButton/LoginNavButton'
 import SignInSignOutButton from "../components/buttons/signInSignOutButton/SignInSignOutButton"
 import ContinueGoogleButton from "../components/buttons/continueGoogleButton/ContinueGoogleButton"
 import ContinueFacebookButton from "../components/buttons/continueFacebookButton/ContinueFacebookButton"
 import Input from "../components/Input/input"
 
-
 function Login() {
+		// as bordas dos inputs estao sem a cor de fundo!!!
 		const navigate = useNavigate();
+		const ref = useRef(null);
+		const ref2 = useRef(null);
 		return (
 		<>
 			<div className='d-flex justify-content-center mt-5'>
 				<img src={logo} height={141} alt="logo"/>
 			</div>
  	   
-			<p className='d-flex justify-content-center font-h3-32-ubuntu login_text mt-5 mb-5'>Login</p>
+			<p className='d-flex justify-content-center font-h2-40-ubuntu login_text mt-5 mb-5'>Login</p>
 
-			<div className='mx-auto text-white'>
+			<div className='mx-auto text-white teste'>
 
-				<div className='container p-5 main'>
-					<form>
-						<div className='row'>
-							<div className="form-group">
-								<label className="mb-2 font-subtitle-16-ubuntu">Email</label>
-								<Input placeholder="name@email.com"/>
-							</div>
-
-							<div className="form-group mt-4 ">
-								<div className="d-flex">
-										<label className="mb-2 font-subtitle-16-ubuntu">Senha</label>
-										<a href='?#' className="link ms-auto font-subtitle-16-ubuntu forgot_password">Esqueceu sua senha?</a>
-								</div>
-								<Input/>
-							</div>
+				<div className='container p-5 main color_gray'>
+					<div className='row color_gray'>
+						<div className="color_gray" onFocus={ () => {
+								ref.current.style.opacity = 1;
+								
+							}} onBlur={ () => {
+								ref.current.style.opacity = 0.8;
+							}}>
+							<label className="mb-2 font-subtitle-16-ubuntu color_gray opacity_text" ref={ref}>Email</label>
+							<Input placeholder="name@email.com" />
 						</div>
-
-						<div className='row m-0 mt-4 mb-3'>
-							<SignInSignOutButton>Entrar</SignInSignOutButton>
+						<div className="mt-4 color_gray" onFocus={ () => {
+								ref2.current.style.opacity = 1;
+								
+							}} onBlur={ () => {
+								ref2.current.style.opacity = 0.8;
+							}}>
+							<div className="d-flex color_gray">
+								<label className="mb-2 font-subtitle-16-ubuntu color_gray opacity_text" ref={ref2}>Senha</label>
+								<a href='?#' className="link ms-auto font-subtitle-12-ubuntu forgot_password color_gray">Esqueceu sua senha?</a>
+							</div>
+							<Input/>
 						</div>
-						<div className='line font-subtitle-16-ubuntu'>ou</div>
-						<div className="mb-2 mt-2"><ContinueGoogleButton/></div>
-						<div className="mb-2 mt-2"><ContinueFacebookButton/></div>
-					</form>
+					</div>
+					<div className='row m-0 mt-4 mb-3 color_gray'>
+						<SignInSignOutButton>Entrar</SignInSignOutButton>
+					</div>
+					<div className='line font-subtitle-16-ubuntu color_gray'>ou</div>
+					<div className="mb-2 mt-2 color_gray"><ContinueGoogleButton/></div>
+					<div className="mb-2 mt-2 color_gray"><ContinueFacebookButton/></div>
 				</div>
 			</div>
-			<div className="bottom_text text-white d-flex justify-content-center font-body-20-roboto mt-4 p-2">Novo por aqui? <a href='?#' className="link cadastro font-body-20-roboto">&nbsp;Cadastre-se</a></div>
+			<div className="bottom_text text-white d-flex justify-content-center font-body-20-700-roboto mt-4 p-2">Novo por aqui? <a href='?#' className="link cadastro font-body-20-700-roboto">&nbsp;Cadastre-se</a></div>
 		</>
 	);
   }
